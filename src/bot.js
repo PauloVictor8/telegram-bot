039ba2740c36e953c3c8ef2c4142f675c00d3f1b
Victor <paulo9.victor.silva@gmail.com>
2022-10-20 19:50:42 -0300

    .

--- src/bot.js
+++ src/bot.js
@@ -1,11 +1,12 @@
-require('dotenv').config()
-import { Telegraf } from 'telegraf';
+const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT);
-
-bot.start((ctx) => {
-    ctx.reply('Hello')
-})
-
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 bot.launch();
 
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
