5d38661d534c4e733909ec602e182c17630f60fa
Victor <paulo9.victor.silva@gmail.com>
2022-10-20 19:43:35 -0300

    .

--- src/bot.js
+++ src/bot.js
@@ -4,7 +4,8 @@ import { Telegraf } from 'telegraf';
 const bot = new Telegraf(process.env.BOT);
 
 bot.command('start', async (ctx) => {
-    await ctx.telegram.sendMessage(ctx.message.chat.id, 'Hello')
+    await ctx.reply('Hello')
 })
 
 bot.launch();
+
